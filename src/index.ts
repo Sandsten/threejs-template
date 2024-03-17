d21f71c387e2dccca5a3f696c6ac794699d6ec52
Sandsten <staffansandberg@outlook.com>
2024-03-17 09:59:10 +0100

    Fix Vite building

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 import { screenSizeManager } from './modules/helpers';
 
